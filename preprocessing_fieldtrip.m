b3b2dcf35417d2234de0bb61b750163e92708c57
magdamalinowska29 <73298429+magdamalinowska29@users.noreply.github.com>
2023-08-24 11:29:47 +0200

    fixed thresholding problem, works on BIDS
    This is Marida's version of the fieldtrip preprocessing and averaging script where she has fixed the problem with artifacts thresholding. I additionally mad ethe script work with our BIDS structure and save the intermediate and final data sets. Also it's now a function and we can call it from our Main script
     main

--- preprocessing_fieldtrip.m
+++ preprocessing_fieldtrip.m
@@ -1,161 +1,144 @@
-%preprocessing and segmenting the data using field trip
+function results=preprocessing_fieldtrip(raw_data, sensors, output_dir)
+
+ft_defaults
 
 
-%read the data
+% Preprocessing and segmenting the data using FieldTrip
+
+
+
+% Read the data
 cfg = [];
-cfg.dataset = 'subject1.bdf';
-%cfg.continuous  = 'yes';  %force data to be read as continous
+cfg.dataset = raw_data;
 data_eeg    = ft_preprocessing(cfg);
 
-%plot data form one channel
-chansel = 1; %specify the channel
+% Plot data from one channel
+chansel = 1; % Specify the channel
 plot(data_eeg.time{1}, data_eeg.trial{1}(chansel, :))
 xlabel('time (s)')
 ylabel('channel amplitude (uV)')
 legend(data_eeg.label(chansel))
 
-%select channels 
+% Select channels 
 cfg = [];
-cfg.channel     = {'A*', 'B*', 'C*', 'D*', 'EXG1', 'EXG2', 'EXG3'};                      % keep channels 1 to 61 and the newly inserted M1 channel
+cfg.channel     = {'A*', 'B*', 'C*', 'D*', 'EXG1', 'EXG2', 'EXG3'};                      
 data_eeg        = ft_preprocessing(cfg, data_eeg);
 
-%rereferencing
+% Rereferencing
 cfg = [];
-cfg.dataset     = 'subject1.bdf';
+cfg.dataset     = raw_data;
 cfg.reref       = 'yes';
 cfg.channel     = {'A*', 'B*', 'C*', 'D*'};
 cfg.refmethod   = 'avg';
 cfg.refchannel  = 'all';
 data_eeg        = ft_preprocessing(cfg);
 
-%EOG
+% Process HEOG
 cfg = [];
-cfg.dataset    = 'subject1.bdf';
+cfg.dataset    = raw_data;
 cfg.channel    = {'EXG1', 'EXG2'};
 cfg.reref      = 'yes';
 cfg.refchannel = 'EXG2';
 data_eogh      = ft_preprocessing(cfg);
-
 data_eogh.label{2} = 'HEOG';
-
 cfg = [];
 cfg.channel = 'HEOG';
 data_eogh   = ft_preprocessing(cfg, data_eogh);
 
+% Process VEOG
 cfg = [];
-cfg.dataset    = 'subject1.bdf';
+cfg.dataset    = raw_data;
 cfg.channel    = {'EXG2', 'EXG3'};
 cfg.reref      = 'yes';
-cfg.refchannel = 'EXG3'
+cfg.refchannel = 'EXG3';
 data_eogv      = ft_preprocessing(cfg);
-
 data_eogv.label{2} = 'VEOG';
-
 cfg = [];
 cfg.channel = 'VEOG';
 data_eogv   = ft_preprocessing(cfg, data_eogv);
 
- 
+% Append all data
 cfg = [];
 data_all = ft_appenddata(cfg, data_eeg, data_eogh, data_eogv);
 
-elec_pos = ft_read_sens('sensors.pol'); %import sensors position froma file
+save(fullfile(output_dir,'data_all'), "data_all")
+
+% Import sensors position from file
+elec_pos = ft_read_sens(sensors);
 
 
-%filtering, HP
-
+% High-pass filter
 cfg = [];
 cfg.hpfilter = 'yes';
-cfg.hpfreq = .1; % can't do 0.1 filter bc poles fall out of the unit circle (?), but can do for example 0.5
-cfg.hpinstabilityfix = 'reduce';%,'split';%,  %this allows for using .1 filter, but why?
+cfg.hpfreq = .1;
+cfg.hpinstabilityfix = 'reduce';
 data_hp = ft_preprocessing(cfg, data_all);
 
-
-%downsample 
-
+% Downsample 
 cfg=[];
 cfg.resamplefs = 200;
 data_downsampled = ft_resampledata(cfg, data_hp);
 
-%filtering, LP
-
+% Low-pass filter
 cfg = [];
 cfg.lpfilter = 'yes';
 cfg.lpfreq = 30;
 data_lp = ft_preprocessing(cfg, data_downsampled);
 
+save(fullfile(output_dir,'data_lp'),"data_lp");
 
-%epoch
-
-%see what event types are in the file
-
+% Epoching
 cfg = [];
-cfg.dataset             = 'subject1.bdf';
+cfg.dataset             = raw_data;
 cfg.trialdef.eventtype = '?';
 dummy                   = ft_definetrial(cfg);
 
-% prepare to segment into epochs around standard trials
+% Prepare to segment into epochs around standard trials
 cfg                    = [];
-cfg.trialdef.prestim   = -0.1;                   % in seconds
-cfg.trialdef.poststim  = 0.4;                   % in seconds
+cfg.trialdef.prestim   = -0.1;                  
+cfg.trialdef.poststim  = 0.4;                  
 cfg.trialdef.eventtype = 'STATUS'; 
-cfg.trialdef.eventvalue =65152;
-cfg.dataset            = 'subject1.bdf';             % set the name of the dataset
-cfg_tr_standard             = ft_definetrial(cfg);   % read the list of the specific stimulus
+cfg.trialdef.eventvalue = 65152;
+cfg.dataset            = raw_data;            
+cfg_tr_standard        = ft_definetrial(cfg);
 
-%prepare to segment into epochs around rare trials
-
-cfg.trialdef.eventtype = 'STATUS';
-cfg.trialdef.eventvalue =65216;
+% Prepare to segment into epochs around rare trials
+cfg.trialdef.eventvalue = 65216;
 cfg_tr_rare             = ft_definetrial(cfg);
 
-% segment data according to the trial definition
-data_trial_standard                   = ft_redefinetrial(cfg_tr_standard, data_lp);
-data_trial_rare                       = ft_redefinetrial(cfg_tr_rare, data_lp);
+% Segment data according to the trial definition
+data_trial_standard    = ft_redefinetrial(cfg_tr_standard, data_lp);
+data_trial_rare        = ft_redefinetrial(cfg_tr_rare, data_lp);
 
+save(fullfile(output_dir,'data_trial_rare'), "data_trial_rare");
+save(fullfile(output_dir,'data_trial_standard'), "data_trial_standard");
 
 %find artifacts- threshold
 
+cfg = [];
+cfg.artfctdef.threshold.min = 80;
+cfg.artfctdef.threshold.max = 100;  
+cfg.channel = 'all';  % Select channels for artifact rejection
+cfg.method = 'threshold';
+data_clean = ft_rejectartifact(cfg, data_trial_standard);
 
-%ERROR: Index in position 2 exceeds array bounds. Index must not exceed 1.
-
-%Error in ft_artifact_threshold (line 197)
-%    dat = ft_fetch_data(data,        'header', hdr, 'begsample', cfg.trl(trlop,1), 'endsample', cfg.trl(trlop,2), 'chanindx', channelindx, 'checkboundary', strcmp(cfg.continuous, 'no'));
- 
-
-
-
-%cfg=[];
-%cfg.dataset    = 'subject1.bdf';
-
-%cfg.trl        = cfg_tr_standard;
-%cfg.continuous = 'no';
-%cfg.artfctdef.threshold.min       = 80;
-
-%[cfg, artifact]= ft_artifact_threshold(cfg, data_trial_standard);
-
-%averaging
-
+save(output_dir, "data_clean");
+% Averaging
 cfg=[];
-%cfg.channel            = 'C15';
-cfg.keeptrials= 'no';
+cfg.keeptrials = 'no';
 average_standard = ft_timelockanalysis(cfg, data_trial_standard);
 
+save(fullfile(output_dir,'average_standard'), "average_standard");
 
-%plot
-%figure
-%pol = -1;     % correct polarity
-%h1 = plot(average_standard.time,average_standard.avg, 'color',[0,0,0.5]);
+% Plot
+figure
+pol = -1;
+h1 = plot(average_standard.time, average_standard.avg, 'color', [0,0,0.5]);
 
 cfg=[];
-%cfg.channel            = 'C15';
 cfg.keeptrials= 'no';
 average_rare = ft_timelockanalysis(cfg, data_trial_rare);
 
-
-
-
-
-
+save(fullfile(output_dir,'average_rare'), "average_rare");
 
 
