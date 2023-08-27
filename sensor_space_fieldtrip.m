6724831e3f33dc10e2a34da92a17c45487ad3489
maridazhupa <131866065+maridazhupa@users.noreply.github.com>
2023-08-27 13:10:48 +0200

    sensor_space_fieldtrip.m

--- sensor_space_fieldtrip.m
+++ sensor_space_fieldtrip.m
@@ -9,22 +9,22 @@ function sensor_space_fieldtrip(output_dir)
 load(fullfile(output_dir, 'data_trial_rare.mat'));
 load(fullfile(output_dir, 'data_trial_standard.mat'));
 % Prepare the design matrix
-num_trials_rare = length(data_trial_rare.trial);
+num_trials_rare     = length(data_trial_rare.trial);
 num_trials_standard = length(data_trial_standard.trial);
-num_total_trials = num_trials_rare + num_trials_standard;
+num_total_trials    = num_trials_rare + num_trials_standard;
 
-design = zeros(1, num_total_trials);
-design(1:num_trials_rare) = 1;
+design  = zeros(1, num_total_trials);
+design(1:num_trials_rare)         = 1;
 design((num_trials_rare + 1):end) = 2;
 
 % Configuration for statistical testing in FieldTrip
 cfg = [];
-cfg.method = 'analytic';  % Use the analytic method for a parametric t-test
+cfg.method    = 'analytic';                 % Use the analytic method for a parametric t-test
 cfg.statistic = 'ft_statfun_indepsamplesT'; % Independent samples T-statistic
-cfg.correctm = 'bonferroni'; % Bonferroni correction for multiple comparisons
-cfg.alpha = 0.05; % Significance level
-cfg.design = design;
-cfg.ivar = 1;   % The index of the design vector containing the independent variable
+cfg.correctm  = 'bonferroni';               % Bonferroni correction for multiple comparisons
+cfg.alpha     = 0.05;                       % Significance level
+cfg.design    = design;
+cfg.ivar      = 1;                          % The index of the design vector containing the independent variable
 
 % Perform the statistical test
 stat = ft_timelockstatistics(cfg, data_trial_rare, data_trial_standard);
